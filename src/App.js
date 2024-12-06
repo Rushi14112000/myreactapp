bfc5a5f494db2015ceb12eddfd52ed92529b8952
Rushi <rushirpatil14@gmail.com>
2024-12-06 12:23:44 +0530

    added alert

--- src/App.js
+++ src/App.js
@@ -3,26 +3,45 @@ import './App.css';
 import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
 
 
 function App() {
-  const [mode, setMode] = useState('dark');
+  const [mode, setMode] = useState('light');
 
   const toggleMode = () => {
-    if (mode === 'dark') {
-      setMode('light');
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = 'grey';
+      showAlert('dark mode has been enabled', 'success')
+
     }
     else {
-      setMode('dark');
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert('light mode has been enabled', 'success')
     }
 
+  }
+
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) =>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
   }
   return (
     <>
       <Navbar title="Navbar" about="AboutUs" mode={mode} toggleMode={toggleMode} />
       <div className="container my-3">
         {/* <TextForm heading="Enter text to analyze"/> */}
-        <About />
+        {/* <About /> */}
+        <Alert alert ={alert}/>
       </div>
     </>
   );
